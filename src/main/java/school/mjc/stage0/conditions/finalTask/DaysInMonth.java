20e5d3445554d6f89b676b6b51a8f28e5803156f
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-29 16:33:17 -0600

    Loops and operators implemented

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,47 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        boolean valid;
+        boolean leap;
+        valid = month <= 12 && year != 0 || month >= 1 && year != 0;
+        leap = year%4 == 0 && month == 2 || year%400 == 0 && month == 2;
+
+        if (!valid) {
+            System.out.println("invalid date");
+        }else if(leap){
+            System.out.println("29");
+
+        }else{
+            switch (month){
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    System.out.println("31");
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println("30");
+                    break;
+                case 2:
+                    System.out.println("28");
+                    break;
+
+            }
+        }
+
+
+
+
+
+
+
+
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int value;
+        value = dividend/divider;
+        value *= divider;
+
+        if (value == 0){
+            System.out.println("division by zero");
+        }else if (value == dividend){
+            System.out.println("can be divided completely");
+        }else{
+            System.out.println("cannot be divided completely");
+        }
     }
 }
