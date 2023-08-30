35235f6ea6387db2ccd7ed2a8c6dae057a68597e
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-29 19:02:41 -0600

    Restructured print conditions

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,16 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
-        
-        int value;
-        value = dividend/divider;
-        value *= divider;
-        if (dividend == 0 || divider == 0){
-            System.out.println("by zero");
-        } else if (value == dividend){
-            System.out.println("can be divided completely");
+
+
+        if (dividend != 0 || divider != 0){
+            int value;
+            value = dividend/divider;
+            value *= divider;
+            System.out.println(value == dividend ? "can be divided completely":"cannot be divided completely");
         }else{
-            System.out.println("cannot be divided completely");
+            System.out.println("division by zero");
+
         }
     }
 }
